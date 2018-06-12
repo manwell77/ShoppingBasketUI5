94d9283006edc5497ced8d2e37a7a6c296d8a62f
Manuel Bellet <manwell77@gmail.com>
2018-06-12 17:41:06 +0200

    0.3
    Settings

--- controller/master.controller.js
+++ controller/master.controller.js
@@ -1,14 +1,23 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
                "sap/ui/model/resource/ResourceModel",
            	   "sap/ui/model/Filter",
-        	   "sap/ui/model/FilterOperator"], 
+        	   "sap/ui/model/FilterOperator",
+        	   'sap/ui/core/Fragment',], 
 		
 	function (Controller,ResourceModel,Filter,FilterOperator) {
 	
 		"use strict";
    
 		return Controller.extend("sap.eni.basket.controller.master", {  						
-		
+
+			_getSettings : function () {
+				if (!this._oSettings) {
+					this._oSettings = sap.ui.xmlfragment("sap.eni.basket.fragment.settings", this);
+					this.getView().addDependent(this._oSettings);
+				}
+				return this._oSettings;
+			},
+			
 			onInit: function () {	 
 
 			},
@@ -25,8 +34,35 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 				else { oFilter = new Filter("SearchKey",FilterOperator.Contains,null); }
 				// filter binding
 				this.byId("masterList").getBinding("items").filter(oFilter);				
+			},
+			
+			onFilter: function (oEvent) {
+				this._getSettings().setFilterSearchCallback(null).setFilterSearchOperator(sap.m.StringFilterOperator.Equal).open();				
+			},
+			
+			onConfirm: function( oEvent ) {
+				// get parameters and initialize sorter
+				var parameters = oEvent.getParameters();
+				var aSorter = [];
+				// grouping
+			    if (parameters.groupItem) {
+			    	switch (parameters.groupItem.getKey()) {
+			    		case "1": aSorter.push(new sap.ui.model.Sorter("CreatedBy",parameters.groupDescending,true)); break;
+			    	}	 
+			    }
+			    if (parameters.sortItem) {
+			    	switch (parameters.sortItem.getKey()) {
+			    		case "1": aSorter.push(new sap.ui.model.Sorter("BasketNumber",parameters.groupDescending,false)); break;
+			    		case "2": aSorter.push(new sap.ui.model.Sorter("CreatedBy",parameters.groupDescending,false)); break;
+			    		default: break;
+			    	}
+			    }
+			    if (aSorter.length > 0) 
+			    	{ this.byId("masterList").getBinding("items").sort(aSorter); this.byId("butFilter").setIcon("sap-icon://add-filter"); }
+			    else
+			    	{ this.byId("butFilter").setIcon("sap-icon://filter"); }
 			}
-		
+			
 		});
    
 });
